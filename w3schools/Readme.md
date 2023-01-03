3e9dd037cb8449658fce0b14e2212ece47d9addf
Rajmohan R <ram.rajmohanr@gmail.com>
2023-01-03 14:28:25 +1100

    updates

---
+++ w3schools/Readme.md
@@ -0,0 +1,12 @@
+# Initialize a Module
+go mod init example.com/hello
+
+- creates go.mod in the current working directory
+
+# Run a Go program
+❯ go run ./helloworld.go
+Hello World!
+
+# Build a Go program
+❯ go build ./helloworld.go
+
---
+++ w3schools/helloworld.go
@@ -0,0 +1,17 @@
+// Package declaration
+package main
+
+// Import packages
+import ("fmt")
+
+
+// Function - Main (entrypoint)
+func main() {
+  // Statements and expressions
+
+  /* Println function from
+  the imported fmt package
+  */
+  fmt.Println("Hello World!")
+  variables()
+}

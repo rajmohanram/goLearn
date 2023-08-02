c18ca77cb0f082289afd3f3f21975d570e289d6c
Rajmohan R <ram.rajmohanr@gmail.com>
2023-08-02 13:05:42 +1000

    upd

--- L01/Hello.go
+++ L01/Hello.go
@@ -1,7 +1,12 @@
+// Package declaration - name of the package
 package main
 
+// package import
 import "fmt"
 
+
+// main function - begining of program execution
 func main() {
    fmt.Println("Hello, World")
 }
+
